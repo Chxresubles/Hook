327bf2d921d2fec386476403ad9da6a2bda7be83
Chxresubles <favre.le@hotmail.com>
2022-10-25 16:10:48 +0200

    ADD: Add routes for the server

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -15,56 +15,111 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 		// Cast the event data to HTTP message
 		struct mg_http_message* hm = (struct mg_http_message*)ev_data;
 
-		// Get the artist and song name from the URI
-		int splits;
-		mg_str* split_array;
-		if (hm->uri.len > 1) {
-			MG_INFO(("SPLIT URI..."));
-			split_array = split(hm->uri, '/', splits);
-			MG_INFO(("DONE."));
-			MG_INFO(("Found %d splits", splits));
-			if (splits == 2) {
-				char* artist = (char*) malloc(split_array[0].len * sizeof(char));
-				char* song_name = (char*)malloc(split_array[1].len * sizeof(char));
-				memcpy(artist, split_array[0].ptr, split_array[0].len);
-				memcpy(song_name, split_array[1].ptr, split_array[1].len);
+		// Route to search musics from the query /search
+		if (mg_http_match_uri(hm, "/search")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"search\"}\n");
 
-				// Get song infos
-				MG_INFO(("GET INFOS OF '%s' by '%s'...", song_name, artist));
-				mp3_infos* infos = mp3_get_infos(artist, song_name);
-				if (infos == nullptr) return;
-				MG_INFO(("Title: %s", infos->title.ptr));
-				MG_INFO(("Artist: %s", infos->artist.ptr));
-				MG_INFO(("Duration: %u", infos->duration));
-				MG_INFO(("Album: %s", infos->album.ptr));
-				MG_INFO(("Date: %s", infos->date.ptr));
-				MG_INFO(("File name: %s", infos->filename.ptr));
-				MG_INFO(("READING MP3 FILE..."));
-				mg_http_serve_file(c, hm, infos->filename.ptr, &opts);
+		// Route to get the next music in line /get/next
+		} else if (mg_http_match_uri(hm, "/get/next")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"get next\"}\n");
 
-				// Free all data
-				MG_INFO(("CLEAN UP INFOS CHAR ARRAYS..."));
-				MG_INFO(("CLEAN UP TITLE..."));
-				free((char*)infos->title.ptr);
-				MG_INFO(("CLEAN UP ARTIST..."));
-				free((char*)infos->artist.ptr);
-				MG_INFO(("CLEAN UP ALBUM..."));
-				free((char*)infos->album.ptr);
-				MG_INFO(("CLEAN UP DATE..."));
-				free((char*)infos->date.ptr);
-				MG_INFO(("CLEAN UP FILENAME..."));
-				free((char*)infos->filename.ptr);
-				MG_INFO(("CLEANED UP INFOS CHAR ARRAYS"));
-				free(infos);
-				MG_INFO(("CLEANED UP INFOS"));
+		// Route to get the previous music in line /get/previous
+		}
+		else if (mg_http_match_uri(hm, "/get/previous")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"get previous\"}\n");
+
+		// Route to get the musics that have been be played and will be played /get/line
+		}
+		else if (mg_http_match_uri(hm, "/get/line")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"get line\"}\n");
+
+		// Route to get the musics from the given query /get
+		}
+		else if (mg_http_match_uri(hm, "/get")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"get\"}\n");
+
+		// Route to list the playlist from the user /list/playlist
+		}
+		else if (mg_http_match_uri(hm, "/list/playlist")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"list playlist\"}\n");
+
+		// Route to list the album from the given artist /list/album?artist=XXX
+		}
+		else if (mg_http_match_uri(hm, "/list/album")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"list album\"}\n");
+
+		// Route to list the artists /list/artist
+		}
+		else if (mg_http_match_uri(hm, "/list/artist")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"list artist\"}\n");
+
+		// Route to get the next music in line /play/next
+		}
+		else if (mg_http_match_uri(hm, "/play/next")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"play next\"}\n");
+
+		// Route to get the previous music in line /play/previous
+		}
+		else if (mg_http_match_uri(hm, "/play/previous")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"play previous\"}\n");
+
+		// Route to get the musics from the given query /play
+		}
+		else if (mg_http_match_uri(hm, "/play")) {
+			mg_http_reply(c, 200, "", "{\"result\": \"play\"}\n");
+		}
+		else {
+			// Get the artist and song name from the URI
+			int splits;
+			mg_str* split_array;
+			if (hm->uri.len > 1) {
+				MG_INFO(("SPLIT URI..."));
+				split_array = split(hm->uri, '/', splits);
+				MG_INFO(("DONE."));
+				MG_INFO(("Found %d splits", splits));
+				if (splits == 2) {
+					char* artist = (char*)malloc(split_array[0].len * sizeof(char));
+					char* song_name = (char*)malloc(split_array[1].len * sizeof(char));
+					memcpy(artist, split_array[0].ptr, split_array[0].len);
+					memcpy(song_name, split_array[1].ptr, split_array[1].len);
+
+					// Get song infos
+					MG_INFO(("GET INFOS OF '%s' by '%s'...", song_name, artist));
+					mp3_infos* infos = mp3_get_infos(artist, song_name);
+					if (infos == nullptr) return;
+					MG_INFO(("Title: %s", infos->title.ptr));
+					MG_INFO(("Artist: %s", infos->artist.ptr));
+					MG_INFO(("Duration: %u", infos->duration));
+					MG_INFO(("Album: %s", infos->album.ptr));
+					MG_INFO(("Date: %s", infos->date.ptr));
+					MG_INFO(("File name: %s", infos->filename.ptr));
+					MG_INFO(("READING MP3 FILE..."));
+					mg_http_serve_file(c, hm, infos->filename.ptr, &opts);
+
+					// Free all data
+					MG_INFO(("CLEAN UP INFOS CHAR ARRAYS..."));
+					MG_INFO(("CLEAN UP TITLE..."));
+					free((char*)infos->title.ptr);
+					MG_INFO(("CLEAN UP ARTIST..."));
+					free((char*)infos->artist.ptr);
+					MG_INFO(("CLEAN UP ALBUM..."));
+					free((char*)infos->album.ptr);
+					MG_INFO(("CLEAN UP DATE..."));
+					free((char*)infos->date.ptr);
+					MG_INFO(("CLEAN UP FILENAME..."));
+					free((char*)infos->filename.ptr);
+					MG_INFO(("CLEANED UP INFOS CHAR ARRAYS"));
+					free(infos);
+					MG_INFO(("CLEANED UP INFOS"));
+				}
+				MG_INFO(("CLEAN UP SPLIT ARRAYS..."));
+				for (int i = 0; i < splits; i++) {
+					free((char*)split_array[i].ptr);
+				}
+				MG_INFO(("CLEAN UP SPLIT ARRAY..."));
+				free(split_array);
+				MG_INFO(("DONE"));
 			}
-			MG_INFO(("CLEAN UP SPLIT ARRAYS..."));
-			for (int i = 0; i < splits; i++) {
-				free((char*)split_array[i].ptr);
-			}
-			MG_INFO(("CLEAN UP SPLIT ARRAY..."));
-			free(split_array);
-			MG_INFO(("DONE"));
 		}
 	}
 }
